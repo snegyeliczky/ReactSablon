46daf093c9f3c1bf30916c2ca6541ee57d758197
Negyeliczky Sandor <sandor.negyeliczky@gmail.com>
2023-01-25 17:44:03 +0100

    add tree

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import logo from './logo.svg';
+import tree from './tree-g5c1c087bb_1280.jpg'
 import './App.css';
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <img src={tree} className="tree" alt="tree" />
     </div>
   );
 }
