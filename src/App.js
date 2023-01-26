ce2bf29c5da95286d60d2d3d75e1fee0b8c77aee
Negyeliczky Sandor <sandor.negyeliczky@gmail.com>
2023-01-26 16:51:11 +0100

    add another page

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import logo from './logo.svg';
 import tree from './tree-g5c1c087bb_1280.jpg'
 import './App.css';
+import {useState} from "react";
 
 function App() {
+    const [answer, setAnswer] = useState('')
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Sziasztok internet Nepe :D LOL
+          Sziasztok internet Nepe :D
+            Miujsag???
         </p>
+          <p>{answer}</p>
+          <textarea onChange={(ans) => {
+              console.log(ans)
+
+          }}/>
+
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -19,6 +28,13 @@ function App() {
           Learn React
         </a>
       </header>
+        <p>
+            <a
+                href={`/go`}
+            >
+                click to go for an other page
+            </a>
+        </p>
       <img src={tree} className="tree" alt="tree" />
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,15 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Root from './roots/Root.tsx'
 import reportWebVitals from './reportWebVitals';
+import ReactDOM from "react-dom/client";
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element:  <App/>,
+    },
+    {
+        path: "/go",
+        element:  <Root/>,
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
 );
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
